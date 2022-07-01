1ca0df34ef8bd40fedc96141f4d7028d42e60e89
Garpsi <Garpsiho@gmail.com>
2022-07-01 23:58:53 +0100

    practice

---
+++ app.js
@@ -0,0 +1,28 @@
+// let arr = [1, 4, 9, 16]
+
+// let newArray = arr.map((element) => {
+//     console.log(element)
+//     return undefined
+// })
+
+// console.log(newArray)
+
+
+let dollars = [1, 5, 10, 3];
+
+let cents = []
+
+for (let i = 0; i < dollars.length; ++i) {
+    dollars.push(dollars[i] * 100)
+}
+
+console.log(cents)
+
+// let cents = dollars.map((element) => {
+//     return element * 100
+// })
+
+// let cents = dollars.map(element => element * 100)
+
+// console.log(cents)
+
