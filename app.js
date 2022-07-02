9a5ebee27a7f5e4a2d5b2ab12be1b7f2b1fa7c51
Garpsi <Garpsiho@gmail.com>
2022-07-02 14:51:28 +0100

    final commit

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// let arr = [1, 4, 9, 16]
-
-// let newArray = arr.map((element) => {
-//     console.log(element)
-//     return undefined
-// })
-
-// console.log(newArray)
-
-
-let dollars = [1, 5, 10, 3];
-
-let cents = []
-
-for (let i = 0; i < dollars.length; ++i) {
-    dollars.push(dollars[i] * 100)
+function toggleDarkMode() {
+    document.querySelector('body').classList.toggle("dark-theme")
 }
-
-console.log(cents)
-
-// let cents = dollars.map((element) => {
-//     return element * 100
-// })
-
-// let cents = dollars.map(element => element * 100)
-
-// console.log(cents)
-
