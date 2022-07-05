8b97b767e7155de0434f31557f5095ef2e34ba88
Garpsi <Garpsiho@gmail.com>
2022-07-05 17:18:27 +0100

    slight update

--- Week 4/finalProject/index.js
+++ Week 4/finalProject/index.js
@@ -1,25 +1,18 @@
 // http://www.omdbapi.com/?apikey=baeb28bc&
 // http://img.omdbapi.com/?apikey=baeb28bc&
 
-// async function onSearch(event) {
-//   const movie = event.target.value
-//   console.log(event)
-// }
 const movieListEl = document.querySelector('.movie__list')
-
-
-async function main() {
-  const movies = await fetch (`http://www.omdbapi.com/?apikey=baeb28bc&s=fast`)
-  const moviesData = await movies.json()
-  movieListEl.innerHTML = moviesData.Search.slice(0, 6).map(movie => movieHTML(movie)).join('')
-}
-
-main()
+const spinnerWrapper = document.querySelector('.spinner__wrapper')
+const noResults = document.querySelector('.search__failure')
 
 async function onSearch(event) {
+
+  spinnerWrapper.classList += ' movies__loading'
   const search = event.target.value
-  const movies = await fetch (`http://www.omdbapi.com/?apikey=baeb28bc&s=${search}`)
+  movies = await fetch (`http://www.omdbapi.com/?apikey=baeb28bc&s=${search}`)
   const moviesData = await movies.json()
+  spinnerWrapper.classList.remove('movies__loading')
+
   movieListEl.innerHTML = moviesData.Search.slice(0, 6).map(movie => movieHTML(movie)).join('')
 }
 
@@ -27,6 +20,13 @@ function movieHTML(movie) {
   return `<div class="movie__card">
   <img class="movie__poster" src="${movie.Poster}" alt="">
   <h3 class="movie__title">${movie.Title}</h3>
-  <h4><i class="fa-solid fa-calendar-days"></i> :${movie.Year}</h4>
+  <h4><i class="fa-solid fa-calendar-days"></i> : ${movie.Year}</h4>
 </div>`
+}
+
+var slider = document.getElementById('myRange')
+var output = document.getElementById('year')
+
+slider.oninput = function(yr) {
+  output.innerHTML = this.value
 }
