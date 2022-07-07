9dc7ff5529f57a794a1efd9b9d6a5b6ef8dc5d8b
Garpsi <Garpsiho@gmail.com>
2022-07-07 22:38:36 +0100

    no more going to server

--- Week 5/reactcrashcourse/tutorial-begin/src/App.js
+++ Week 5/reactcrashcourse/tutorial-begin/src/App.js
@@ -1,14 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Navbar'
 import Home from './Home'
+import Create from './Create';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar /> 
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar /> 
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/create" element={<Create/>}/>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
