f9eab4b3b0c07009bcc46f792839e0d3016b6f72
Garpsi <Garpsiho@gmail.com>
2022-07-08 16:01:43 +0100

    Complete backbone blog

--- Week 5/reactcrashcourse/tutorial-begin/src/App.js
+++ Week 5/reactcrashcourse/tutorial-begin/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Navbar'
 import Home from './Home'
 import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home/>}/>
             <Route exact path="/create" element={<Create/>}/>
+            <Route exact path="/blogs/:id" element={<BlogDetails/>}/>
+            <Route exact path='*' element={<NotFound/>}/>
           </Routes>
         </div>
       </div>
